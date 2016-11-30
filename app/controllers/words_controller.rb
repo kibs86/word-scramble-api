47c9de9967640a5739858b12a31ede3003a18daa
kibs86 <kibs086@gmail.com>
2016-11-30 16:31:47 -0500

    modify getword to check completed words
    - added functionality to getword to check completed words before
      returning one to the user

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,8 @@
 class WordsController < OpenReadController
   # class WordsController < ApplicationController
   before_action :set_word, only: [:update, :destroy]
+  before_action :fetch_comp_words, only: [:getword]
+  before_action :fetch_cust_response, only: [:getword]
 
   # GET /words
   # GET /words.json
@@ -16,9 +18,33 @@ class WordsController < OpenReadController
     render json: @word
   end
 
+  def fetch_comp_words
+    # get a list of all completed words
+    @comp_words = \
+      Word.joins(:completed_words).where('completed_words.user_id' => current_user).ids
+  end
+
+  def fetch_cust_response
+    @custom_response = { word: { id: '', word: '', creator: '', difficulty: ''} }
+  end
+
   def getword
+    # get difficulty from parameters
     @diff = params[:difficulty]
-    @words = Word.where('difficulty' => @diff).ids
+
+    # get a list of all words with that difficulty tier
+    @diff_words = Word.where('difficulty' => @diff).ids
+
+    # remove ids from @words that are in @comp_words
+    @words = []
+    @diff_words.each do |x|
+      @words.push(x) if @comp_words.exclude?(x)
+    end
+
+    # if words array is empty, send back custom response for error handling
+    return render json: @custom_response if @words.empty?
+
+    # from that list, get one at random to return
     @word = Word.find(@words[rand(@words.length)])
     render json: @word
   end
